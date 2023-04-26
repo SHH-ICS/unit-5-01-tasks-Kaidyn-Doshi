ac7d2e610d6899e3406f14ebeb23f5b616dba3b8
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-04-26 11:28:22 -0400

    Update Q2RandomRange.py

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,17 @@
-# Create a program that accepts 2 numbers from the user. 
-# Your program will output a random number between the range given by the user.
+
+import random
+
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        if num1 > num2:
+            print("Invalid input! The first number should be smaller than or equal to the second number.")
+            continue
+        break
+    except ValueError:
+        print("Invalid input! Please enter numbers only.")
+
+random_num = random.uniform(num1, num2)
+
+print(f"A random number between {num1} and {num2} is {random_num:.1f}")
