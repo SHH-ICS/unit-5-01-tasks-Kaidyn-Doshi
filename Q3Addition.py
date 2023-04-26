6a09406e8628e1008cfb790cbb0010ca55d6e37c
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-04-26 11:28:40 -0400

    Update Q3Addition.py

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,2 +1,14 @@
-# Create a program that will ask the user an addition question. 
-# The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
+import random
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 100)
+while True:
+    try:
+        answer = float(input(f"What is {num1} + {num2}? "))
+        if answer != num1 + num2:
+            print("😥 wrong!!")
+            continue
+        break
+    except ValueError:
+        print("Invalid input! Please enter numbers only.")
+
+print("You are correct 👍!")
