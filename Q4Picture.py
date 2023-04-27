79ba25fa4186d9401d212467e4d11d85d17be426
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-04-26 21:52:28 -0400

    Update Q4Picture.py

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,16 +1,13 @@
 import pygame
-
 pygame.init()
-
 screen = pygame.display.set_mode((720, 700))
 
+# Shield
 shield_rect = pygame.draw.ellipse(screen, (220, 20, 60), (220, 115, 200, 250))
 pygame.draw.ellipse(screen, (70, 70, 70), shield_rect, 5)
-
 symbol_center = (shield_rect.centerx, shield_rect.centery - 8)
 pygame.draw.circle(screen, (219, 172, 52), symbol_center, 50)
 pygame.draw.circle(screen, (70, 70, 70), symbol_center, 50, 2)
-
 for angle in range(0, 360, 45):
     start_pos = pygame.math.Vector2(symbol_center) + pygame.math.Vector2(0, -40).rotate(angle)
     end_pos = start_pos + pygame.math.Vector2(0, -25).rotate(angle)
@@ -18,12 +15,26 @@ for angle in range(0, 360, 45):
 
 for angle_start, angle_end in [(45, 90), (135, 180), (225, 270), (315, 360)]:
     pygame.draw.arc(screen, (219, 172, 52), (symbol_center[0] - 30, symbol_center[1] - 30, 60, 60), angle_start, angle_end, 10)
-
 pygame.draw.circle(screen, (150, 150, 150), symbol_center, 30)
 pygame.draw.circle(screen, (50, 50, 50), symbol_center, 50, 2)
 
-pygame.display.update()
 
+# Sword
+sword_x = screen.get_width() // 4 + 300
+sword_y = screen.get_height() // 2 + 50
+pygame.draw.polygon(screen, (70, 70, 70), [(sword_x - 25 // 2, sword_y), 
+                                           (sword_x + 25 // 2, sword_y),
+                                           (sword_x + 25 // 2, sword_y - 80),
+                                           (sword_x - 25 // 2, sword_y - 80)])
+pygame.draw.polygon(screen, (0, 255, 160), [(sword_x - 100 // 2, sword_y - 80 - 20),
+                                            (sword_x + 100 // 2, sword_y - 80 - 20),
+                                            (sword_x + 100 // 2, sword_y - 80),
+                                            (sword_x - 100 // 2, sword_y - 80)])
+pygame.draw.polygon(screen, (0, 255, 255), [(sword_x - 20 // 2, sword_y - 80 - 20 - 250),
+                                            (sword_x + 20 // 2, sword_y - 80 - 20 - 250),
+                                            (sword_x + 20 // 2, sword_y - 80 - 20),
+                                            (sword_x - 20 // 2, sword_y - 80 - 20)])
+pygame.display.update()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
